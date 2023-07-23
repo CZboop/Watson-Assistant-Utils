22985251c61a2190d8db1b0c1d184e5a4733bb43
Branchey <70140890+CZboop@users.noreply.github.com>
2023-07-23 10:13:03 +0100

    styled the file upload button to fit the rest of the page styling rather than the native default file upload button

--- web-app/src/components/FileUploader.js
+++ web-app/src/components/FileUploader.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 import './FileUploader.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUpload } from '@fortawesome/free-solid-svg-icons'
+
 
 function FileUploader() {
     const [files, setFiles] = useState(null);
+    const [fileName, setFileName] = useState("");
     
     const handleFile = (e) => {
         const fileReader = new FileReader();
         fileReader.readAsText(e.target.files[0], "UTF-8");
+        let fileName = e.target.files[0].name;
+        console.log(fileName);
         fileReader.onload = e => {
             let fileUploaded = e.target.result;
             setFiles(fileUploaded);
+            setFileName(fileName);
         };
     }
     useEffect(() => {
@@ -23,8 +30,14 @@ function FileUploader() {
     <div className='FileUploader'>
         <h3>Upload JSON File</h3>
         <div className='UploadForm'>
-            <input className="FileInput" type="file" name="FileInput" onChange={handleFile}/>
-            
+            <input className="FileInput" type="file" name="FileInput" onChange={handleFile} id="uploadFile"/>
+            <label htmlFor="uploadFile" className="fileUploaderButton">Upload File<FontAwesomeIcon className="uploadIcon" icon={faUpload} /></label>
+            {
+                fileName !== "" ?
+                <p>Selected File: {fileName}</p>
+                :
+                <p></p>
+            }
         </div>
         <form>
         <input type="submit" value="Submit" className="submit-file"/>
