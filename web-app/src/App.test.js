8696c72557ce406a13b125b0112b38d034e78374
Branchey <70140890+CZboop@users.noreply.github.com>
2023-07-24 08:25:03 +0100

    added app renders smoke test and navigation tests for react app

--- web-app/src/App.test.js
+++ web-app/src/App.test.js
@@ -1,5 +1,54 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
 import App from './App';
 
-test('', () => {
-});
+// TODO: test render and navigation
+
+it('App component renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+  });
+  
+// testing navigation
+test('Initially loads home page', async () => {
+    const div = document.createElement('div');
+    render(<App />, div);
+    const user = userEvent.setup();
+
+    // verify home page content
+    expect(screen.getByText(/Welcome! This is a page hosting some simple tools/i)).toBeInTheDocument();
+})
+
+// verify jump to finder link and resulting page header
+test('Able to navigate to jump to finder', async () => {
+    const div = document.createElement('div');
+    render(<App />, div);
+    const user = userEvent.setup();
+    await user.click(screen.getByRole('link', { name: 'Jump-To Finder' }));
+    expect(screen.getByRole("heading", { level: 2 }).textContent).toEqual("Jump-To Finder");
+})
+
+
+// verify node intent finder link and resulting page header
+test('Able to navigate to node intent finder', async () => {
+    const div = document.createElement('div');
+    render(<App />, div);
+    const user = userEvent.setup();
+    await user.click(screen.getByRole('link', { name: 'Node Intent Finder' }));
+    expect(screen.getByRole("heading", { level: 2 }).textContent).toEqual("Node Intent Finder");
+})
+
+// verify intent node finder link and resulting page header
+test('Able to navigate to intent node finder', async () => {
+    const div = document.createElement('div');
+    render(<App />, div);
+    const user = userEvent.setup();
+    await user.click(screen.getByRole('link', { name: 'Intent Node Finder' }));
+    expect(screen.getByRole("heading", { level: 2 }).textContent).toEqual("Intent Node Finder");
+})
+
+// test('', () => {
+// });
